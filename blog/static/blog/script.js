f36ec10c6a1e203b82c743b0d934c7cd92ee354e
Little Blue <blue@Littles-MacBook-Air.local>
2020-06-21 12:28:32 -0700

    file management update

--- blog/static/blog/script.js
+++ blog/static/blog/script.js
@@ -1,55 +1,142 @@
 $(function() {
-  // Hide an element
-  $(".red-box").hide();
-
-  // Show it again
-  $(".red-box").show();
-
-  // Just toggle visibility depending on whether element is shown or hidden
-  $(".red-box").toggle();  // hides
-
-  // Up until this point, you can't even see what happens in the browser
-  // because it happens in a matter of milliseconds.
-
-  // Use show/hide as animations (not very commonly used)
-  $(".green-box").hide(1000);
-  $(".green-box").show(1000);
-
-  $('.blue-box').delay(1000).animate({
-    'margin-left': '+=100px'
-  }, 1500, () => {
-    $('.red-box').toggle(1000);
-  });
-
-  var csrfToken = $('input[name=csrfmiddlewaretoken]').val();
-  
-  $('.comment-form').on('submit', e => {
-    e.preventDefault();
-    const pk = e.target.dataset.indexNumber;
-    postComment(pk);
-  });
-
-  const postComment = function(pk) {
-    const text = $(`#comment-${pk}`).val();
-
-    $.ajax({
-      url: `post/${pk}/`,
-      type: 'POST',
-      data: {
-        csrfmiddlewaretoken: csrfToken,
-        commentText: text
-      },
-      success: json => {
-        $(`#comment-${pk}`).val('');
-        console.log(json);
-        $("#talk").prepend(`<li><strong>${json.text}</strong> - <em>${json.author}</em></li>`);
-      },
-      error: (xhr, errmsg, err) => {
-        console.log(errmsg);
-      }
-    });
-  };
+	const csrfToken = $('input[name=csrfmiddlewaretoken]').val();
 
 
+	$(document).ready(function() {
+	    $('.likes-form').each(function() {
+	    	const url = $(this)[0].dataset.url;
+	      	const pk = $(this)[0].dataset.indexNumber;
+	      	const serializeData = $(`.likes-form[data-index-number="${pk}"]`).serialize();
 
+	      	$.ajax({
+		        url: url,
+		        type: 'GET',
+		        data: serializeData,
+		        success: json => {
+		          	if (json.current === 'blank') {
+		            	$(`.likes-img[data-index-number="${pk}"]`).attr('src', '/static/blog/img/heart-outline.svg');
+		          	} else if (json.current === 'liked') {
+		            	$(`.likes-img[data-index-number="${pk}"]`).attr('src', '/static/blog/img/heart-solid-pink.svg');
+		          	}
+		        },
+		        // console the error message
+		        error: (xhr, errmsg, err) => {
+		          	console.log(errmsg);
+		        }
+	      	});
+	    });
+  	});
+
+
+	const toggleLike = pk => {
+		const url = $(`.likes-form[data-index-number="${pk}"]`).data('url');
+	    const serializeData = $(`.likes-form[data-index-number="${pk}"]`).serialize();
+
+	    $.ajax({
+	        url: url,
+	        type: 'POST',
+	        data: serializeData,
+	        success: json => {
+	        	if (json.status === 'removed') {
+	        		$(`.likes-img[data-index-number="${pk}"]`).attr('src', '/static/blog/img/heart-outline.svg');
+	        	} else if (json.status === 'added') {
+	        		$(`.likes-img[data-index-number="${pk}"]`).attr('src', '/static/blog/img/heart-solid-pink.svg');
+	        	}
+	    	},
+	    	// console the error message
+	      	error: (xhr, errmsg, err) => {
+	        	console.log(errmsg);
+	    	}
+	  	});
+	};
+
+
+	const postBtnState = (input, btn) => {
+		if ($(input).val()) {
+			$(btn).prop('disabled', false);
+			$(btn).removeClass('disabled');
+		} else {
+			$(btn).prop('disabled', true);
+			$(btn).addClass('disabled');
+		}
+	}
+
+
+	$('.likes-btn').on('click', e => {
+	    e.preventDefault();
+	    const pk = e.target.dataset.indexNumber;
+	    toggleLike(pk);
+	});
+
+
+	$('.comment-input').on('input', function(e) {
+		const btn = $(this)[0].parentNode.lastElementChild;
+
+		if ($(this).val()) {
+			$(btn).prop('disabled', false);
+			$(btn).removeClass('disabled');
+		} else {
+			$(btn).prop('disabled', true);
+			$(btn).addClass('disabled');
+		}
+	});
+
+
+	const newComment = (pk, value) => {
+		const url = $(`.comment-form[data-index-number="${pk}"]`).data('url');
+
+		$.ajax({
+			url: url,
+			type: 'POST',
+			data: {
+				csrfmiddlewaretoken: csrfToken,
+				commentText: value
+			},
+			success: json => {
+				const input = $(`#comment-${pk}`).val('');
+				const btn = $(`#comment-${pk}`)[0].parentNode.lastElementChild;
+
+				postBtnState(input, btn);
+
+				const markup = `
+					<li class="list-group-item px-3 py-1 border-0">
+						<span class="text-muted mr-2 text-uppercase font-weight-bold">${json.author}</span>${json.text}
+					</li>
+				`;
+
+				const markup2 = `
+					<li class="list-group-item px-3 p-0 mb-3 border-0">
+	                    <img src="${json.url}" alt="" class="rounded-circle article-img mr-2" draggable="false">
+	                    <span class="text-muted mr-2 text-uppercase font-weight-bold">${json.author}</span>
+	                    ${json.text}
+	                </li>
+				`;
+
+				$(`ul[data-index-number="${pk}"]`).append(markup);
+				$('.detail-ul').prepend(markup2);
+			}
+		});
+	}
+
+
+	$('.comment-form').on('submit', function(e) {
+		e.preventDefault();
+		const pk = e.target.dataset.indexNumber;
+		const text = e.currentTarget[1].value;
+		newComment(pk, text);
+	});
+
+
+	$('.bookmark-btn').on('click', e => {
+		e.preventDefault();
+		const pk = e.currentTarget.firstElementChild.dataset.indexNumber;
+		const obj = $(e.currentTarget.firstElementChild).attr('src');
+
+		if (obj === '/static/blog/img/bookmark-outline.svg') {
+			$(`.bookmark-img[data-index-number="${pk}"]`).attr('src', '/static/blog/img/bookmark-solid-black.svg');
+		} else {
+			$(`.bookmark-img[data-index-number="${pk}"]`).attr('src', '/static/blog/img/bookmark-outline.svg');
+		}
+		
+	});
 });
